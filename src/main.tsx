2095e99fcb07b7eee1e7b7b368376da739b9e3a8
Arda Akcagöz <a.akcgz002@hotmail.com>
2024-01-16 14:32:13 +0300

    Minor Changes at App.tsx and Added AppBarComponent.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { MemoryRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { green, blue, grey, common, lightBlue, purple } from "@mui/material/colors";
+import { AppBarComponent } from "./AppBarComponent";
+import { Home } from "./Home";
+import { Settings } from "./Settings";
 
+const theme = createTheme({
+    palette: {
+        primary: lightBlue,
+        secondary: purple,
+        background: {
+            default: grey[100],
+        },
+        mode: "light",
+    }
+});
+
+function Root() {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <AppBarComponent/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/settings" element={<Settings />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <MemoryRouter>
-            <App />
-        </MemoryRouter>
+        <Root />
     </React.StrictMode>
 );
